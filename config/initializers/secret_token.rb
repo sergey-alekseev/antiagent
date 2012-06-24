393eb76a5edb04ba443a5bf4a3b4191bb4afdf30
Sergey Alekseev <by.bsu.alex@gmail.com>
2012-06-24 15:43:09 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antiagent::Application.config.secret_token = '32ecf16d9d4f3b5a2e8ef76ceb10799dee56191b3fdcb021cd5c242c866d7d29bf526ccf8bd50cae6faffb8a70413d79a6dd40d7c4155eb4aab6ae09e32bb166'
